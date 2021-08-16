fec19286ddc1707d5189c84498b673e98ce90406
marinerbob <robobson@mail.ru>
2021-08-16 16:42:53 +0300

    removed redundant loader from webpack config

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -32,14 +32,6 @@ module.exports = {
                 },
             },
             {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
                 test: /\.(woff(2)?|ttf|eot|png|svg|jpg|gif|pdf|ico)$/,
                 type: 'asset/resource'
             }
